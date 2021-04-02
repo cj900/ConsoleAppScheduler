05ce1cf9baca1c1967acecfa8623436d09689840
Jake <admin@admin.com>
2021-04-02 15:17:23 +0800

    Update Log Method

--- ConsoleAppScheduler/AppEntity.cs
+++ ConsoleAppScheduler/AppEntity.cs
@@ -12,18 +12,20 @@ namespace ConsoleAppScheduler
         public string LogPrefix { get; set; }
         public bool ErrorOutput { get; set; }
         public bool StandardOutput { get; set; }
-
+        public string LogFile { get; set; }
         public void Log(string msg)
         {
             if (!String.IsNullOrWhiteSpace(msg))
             {
-                Logs.WriteCurrent($"[{LogPrefix}] {msg}");
+                //Logs.WriteCurrent($"[{LogPrefix}] {msg}");
+                Logs.Write(LogFile,$"[{LogPrefix}] {msg}");
             }
         }
 
         public void Log(Exception e)
         {
-            Logs.WriteCurrent($"[{LogPrefix}][Error] "+e.GetExceptionFootprints());
+          //  Logs.WriteCurrent($"[{LogPrefix}][Error] "+e.GetExceptionFootprints());
+          Logs.Write(LogFile,$"[{LogPrefix}][Error] "+e.GetExceptionFootprints());
 
         }
     }
